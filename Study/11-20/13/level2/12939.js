4e03898cf4edbbe17a1ef78a918f6599b8cc42d8
PCY71 <skypcy71@gmail.com>
2022-09-07 22:29:41 +0900

    solve 1 level2 question

---
+++ Study/11-20/13/level2/12939.js
@@ -0,0 +1,12 @@
+
+/* 12939번 문제
+최댓값과 최솟값*/
+
+function solution(s) {
+    let answer = s.split(' ')
+    return `${Math.min(...answer)} ${Math.max(...answer)}`;
+}
+
+console.log(solution("1 2 3 4"));
+console.log(solution("-1 -2 -3 -4"));
+console.log(solution("-1 -1"));
