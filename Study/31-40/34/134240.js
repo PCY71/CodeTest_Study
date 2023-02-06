25102e218ad72e66fd90c90c97ea074e40d4a11e
PCY71 <skypcy71@gmail.com>
2023-02-06 22:45:29 +0900

    solve string parsing problem

---
+++ Study/31-40/34/134240.js
@@ -0,0 +1,15 @@
+function solution(food) {
+    let table = '';
+    for (let i = 1; i < food.length; i++) {
+        const count = Math.floor(food[i] / 2);
+        table += i.toString().repeat(count);
+    }
+
+    return table + '0' + table.split('').reverse().join('');
+}
+
+const test1 = [1, 3, 4, 6];
+const test2 = [1, 7, 1, 2];
+
+console.log(solution(test1));
+console.log(solution(test2));
